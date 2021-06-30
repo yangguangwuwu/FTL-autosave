a4fa66127fa2926fd2573ca6de51597d6930bc8f
五五 <15093565100@163.com>
2021-06-30 09:13:24 +0800

    v2.0

--- go.mod
+++ go.mod
@@ -1,8 +1,12 @@
-module FTLautoSave
+module win
 
 go 1.16
 
 require (
-	github.com/robfig/cron v1.2.0 // indirect
+	fyne.io/fyne/v2 v2.0.3
+	github.com/go-gl/gl v0.0.0-20210501111010-69f74958bac0 // indirect
+	github.com/go-vgo/robotgo v0.94.1 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/robotn/gohook v0.30.6 // indirect
+	github.com/srwiley/oksvg v0.0.0-20200311192757-870daf9aa564 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/driver/desktop"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
 	"github.com/robfig/cron/v3"
+	"image/color"
 	"io"
 	"log"
 	"os"
@@ -10,12 +19,67 @@ import (
 	"time"
 )
 
+//go:embed fonts/msyhbd.ttc
+var FontBold []byte
+
+//go:embed fonts/msyhl.ttc
+var FontLight []byte
+
+//go:embed fonts/msyh.ttc
+var FontRegular []byte
+
+type CnTheme struct {}
+
+var _ fyne.Theme = (*CnTheme)(nil)
+
+func (r CnTheme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
+	if name == theme.ColorNameBackground {
+		if variant == theme.VariantLight {
+			return color.White
+		}
+		return color.Black
+	}
+
+	return theme.DefaultTheme().Color(name, variant)
+}
+
+func (r CnTheme) Icon(name fyne.ThemeIconName) fyne.Resource {
+	return theme.DefaultTheme().Icon(name)
+}
+
+func (r CnTheme) Font(style fyne.TextStyle) fyne.Resource {
+	if style.Monospace {
+		return fyne.NewStaticResource("regular.ttc",FontRegular)
+	}
+	if style.Bold {
+		return fyne.NewStaticResource("bold.ttc",FontBold)
+	}
+	return fyne.NewStaticResource("regular.ttc",FontRegular)
+}
+
+func (r CnTheme) Size(name fyne.ThemeSizeName) float32 {
+	return theme.DefaultTheme().Size(name)
+}
+
 func main() {
+	go initCron()
+	initKey()
+	initWindow()
+}
+
+func initKey() {
+}
+
+func initCron()  {
 	c := cron.New()
 
 	c.AddFunc("*/1 * * * *", func() { //每分钟
-		dirName := GetDirName()
+		dir := "auto"
+		dirName := dir + "/" + GetDirName()
 		if IsPathExists(dirName) == false {
+			if IsPathExists(dir) == false {
+				os.Mkdir(dir, os.ModePerm)
+			}
 			os.Mkdir(dirName, os.ModePerm)
 		}
 		timeDate := GetTimeDate("%d-%02d-%02d-%02d-%02d-%02d")
@@ -24,19 +88,70 @@ func main() {
 			timeDate = GetTimeDate("%d-%02d-%02d %02d:%02d:%02d")
 			log.Printf("FTL自动存档成功！%s ！", timeDate)
 		}
-		//files := GetFilelist(".")
-		//fmt.Println(files)
 	})
 
 	c.Start()
-
-	for {
-		time.Sleep(time.Second)
-	}
 }
 
-func GetFilelist(path string) []string {
-	var files []string
+func initWindow()  {
+	App := app.New()
+	App.Settings().SetTheme(CnTheme{})
+
+	Window := App.NewWindow("FLT存档管理")
+	Window.Resize(fyne.NewSize(400, 300))
+
+	ctrlTab := desktop.CustomShortcut{KeyName: fyne.KeyTab, Modifier: desktop.ControlModifier}
+	Window.Canvas().AddShortcut(&ctrlTab, func(shortcut fyne.Shortcut) {
+		log.Println("We tapped Ctrl+Tab")
+	})
+
+
+	toolbar := widget.NewToolbar(
+		widget.NewToolbarAction(theme.DocumentSaveIcon(), func() {
+			log.Println("save ")
+		}),
+		widget.NewToolbarAction(theme.FolderOpenIcon(), func() {
+			log.Println("load ")
+		}),
+		widget.NewToolbarSpacer(),
+		widget.NewToolbarAction(theme.HelpIcon(), func() {
+			log.Println("Display help")
+		}),
+	)
+	grid := container.NewBorder(
+		toolbar,
+		nil,
+		nil,
+		nil,
+		container.NewGridWithColumns(2,AutoSaveLog(),SaveLog()),
+	)
+	Window.SetContent(grid)
+	Window.ShowAndRun()
+}
+
+func AutoSaveLog() fyne.CanvasObject {
+	files := GetFilelist("auto")
+	log.Println(files)
+
+
+	list := widget.NewListWithData(func() int {
+		return len(files)
+	}, func() fyne.CanvasObject {
+		return container.NewBorder(nil,nil,nil,widget.NewButton("载入", func() {
+		}),widget.NewLabel("test"))
+	}, func(i binding.DataItem , o fyne.CanvasObject) {
+
+	})
+
+	return widget.NewCard("自动存档","",list)
+}
+
+func SaveLog() fyne.CanvasObject  {
+	return widget.NewCard("手动存档","",nil)
+}
+
+func GetFilelist(path string) []map[string]interface{} {
+	var files []map[string]interface{}
 	err := filepath.Walk(path, func(path string, f os.FileInfo, err error) error {
 		if f == nil {
 			return err
@@ -44,8 +159,11 @@ func GetFilelist(path string) []string {
 		if f.IsDir() {
 			return nil
 		}
-		files = append(files, path)
-		//println(path)
+		files = append(files, map[string]interface{}{
+			"time":f.ModTime().Unix(),
+			"fileName":f.Name(),
+			"path":path,
+		})
 		return nil
 	})
 	if err != nil {
